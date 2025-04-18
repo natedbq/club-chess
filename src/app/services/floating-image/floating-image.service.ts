62fcb82836c660075c9c2b3e2e821de38f4f53eb
natedbq <nateallendbq@gmail.com>
2025-04-18 08:56:39 -0500

    summary-fen

--- src/app/services/floating-image/floating-image.service.ts
+++ src/app/services/floating-image/floating-image.service.ts
@@ -10,10 +10,12 @@ export class FloatingImageService {
   private _visible = new BehaviorSubject<boolean>(false);
   private _src = new BehaviorSubject<string>('');
   private _position = new BehaviorSubject<{ top: number; left: number }>({ top: 0, left: 0 });
+  private _evaluation = new BehaviorSubject<number | null>(0);
 
   visible$ = this._visible.asObservable();
   src$ = this._src.asObservable();
   position$ = this._position.asObservable();
+  evaluation$ = this._evaluation.asObservable();
 
   constructor(private router: Router) {
     this.router.events.subscribe(event => {
@@ -23,17 +25,14 @@ export class FloatingImageService {
     });
   }
 
-  showImage(src: string, top: number, left: number) {
+  showImage(src: string, top: number, left: number, evaluation: number | null = null) {
     this._src.next('/assets/emotes/' + src);
     this._position.next({ top, left });
     this._visible.next(false);
+    this._evaluation.next(evaluation);
     setTimeout(() => {
       this._visible.next(true);
     }, 10);
-  }
-
-  moveImage(top: number, left: number) {
-    this._position.next({ top, left });
   }
 
   hideImage() {
